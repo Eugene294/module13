95013afba66a63b428663c08617485b37fe258fe
Eugene294 <koblia.eo@gmail.com>
2024-07-24 23:36:56 +1000

    добавил файлы задач

--- task13_6_1/Program.cs
+++ task13_6_1/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace task13_6_1
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace task13_6_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> list = new List<string>();
+            string path = @"C:\\Users\\Евгений\\Desktop\\курс\\module13\\task13_6_1\\Text1.txt";
+
+            StreamReader sr = new StreamReader(path);
+            while (!sr.EndOfStream)
+            {
+                list.Add(sr.ReadLine());
+            }
+            sr.Close();
+
+            LinkedList<string> link = new LinkedList<string>(list);
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            list.Insert(13400, "добавленная строка");
+            sw.Stop();
+            Console.WriteLine($"Скорость вставки в List<T> = {sw.ElapsedTicks} тактов");
+
+            sw.Restart();
+            link.AddLast("добавленная строка");
+            sw.Stop();
+            Console.WriteLine($"Скорость вставки в LinkedList<T> = {sw.ElapsedTicks} тактов");
         }
     }
 }
---
+++ task13_6_2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace task13_6_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\\Users\\Евгений\\Desktop\\курс\\module13\\task13_6_1\\Text1.txt";
+            StringBuilder text = new StringBuilder();
+
+            StreamReader sr = new StreamReader(path);
+            while (!sr.EndOfStream)
+            {
+                text.Append(sr.ReadLine());
+            }
+            sr.Close();
+
+            string t = text.ToString();
+            var noPunctuationText = new string(t.Where(c => !char.IsPunctuation(c)).ToArray());
+            string[] array = noPunctuationText.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            
+            var most = array.GroupBy(item => item).OrderByDescending(c => c.Count()).Select(grp => new { Word = grp.Key, Count = grp.Count() });
+
+            int i = 0;
+            foreach (var item in most)
+            {
+                i++;
+                if (i == 1 || i == 5)
+                {
+                    Console.WriteLine($"{i}-е в списке самых часто повторяющихся в списке слов это {item.Word}, повторено {item.Count} раз");
+                }
+                else
+                {
+                    Console.WriteLine($"{i}-е в списке самых часто повторяющихся в списке слов это {item.Word}, повторено {item.Count} раза");
+                }
+                if (i == 5) break;
+            }
+        }
+    }
+}
